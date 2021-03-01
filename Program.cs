becddb5731a9cab246ee5018e06e3b1cd5e225d4
Ryan Youngblood <rryb77@gmail.com>
2021-03-01 15:30:45 -0600

    Added the cheater option for infinite guesses

--- Program.cs
+++ Program.cs
@@ -16,6 +16,7 @@ namespace guessinggame
             Console.WriteLine("| 1. Easy                        |");
             Console.WriteLine("| 2. Medium                      |");
             Console.WriteLine("| 3. Hard                        |");
+            Console.WriteLine("| 4. Cheater                     |");
             Console.WriteLine("----------------------------------");
 
             string difficulty = Console.ReadLine();
@@ -36,35 +37,65 @@ namespace guessinggame
 
             Console.WriteLine("Can you guess the secret number? ");
 
-            while (totalGuesses > 0 && winner == false)
+            if (parsedDifficulty <= 3)
             {
-
-                Console.Write($"Your Guess ({totalGuesses}): ");
-                string input = Console.ReadLine();
-                int parsedInput = Int32.Parse(input);
-
-                if (parsedInput == secretNumber)
+                while (totalGuesses > 0 && winner == false)
                 {
-                    Console.WriteLine($"Correct! It was {secretNumber}!");
-                    winner = true;
-                }
-                else
-                {
-                    if (parsedInput > secretNumber)
+
+                    Console.Write($"Your Guess ({totalGuesses}): ");
+                    string input = Console.ReadLine();
+                    int parsedInput = Int32.Parse(input);
+
+                    if (parsedInput == secretNumber)
                     {
-                        Console.WriteLine($"Wrong! Try lower... {secretNumber}");
+                        Console.WriteLine($"Correct! It was {secretNumber}!");
+                        winner = true;
                     }
                     else
                     {
-                        Console.WriteLine($"Wrong! Try higher... {secretNumber}");
+                        if (parsedInput > secretNumber)
+                        {
+                            Console.WriteLine($"Wrong! Try lower...");
+                        }
+                        else
+                        {
+                            Console.WriteLine($"Wrong! Try higher...");
+                        }
                     }
 
+                    totalGuesses--;
+                    Console.WriteLine("");
                 }
-
-                totalGuesses--;
-                Console.WriteLine("");
             }
+            else
+            {
+                while (winner == false)
+                {
 
+                    Console.Write($"Your Guess (∞): ");
+                    string input = Console.ReadLine();
+                    int parsedInput = Int32.Parse(input);
+
+                    if (parsedInput == secretNumber)
+                    {
+                        Console.WriteLine($"Correct! It was {secretNumber}!");
+                        winner = true;
+                    }
+                    else
+                    {
+                        if (parsedInput > secretNumber)
+                        {
+                            Console.WriteLine($"Wrong! Try lower...");
+                        }
+                        else
+                        {
+                            Console.WriteLine($"Wrong! Try higher...");
+                        }
+                    }
+
+                    Console.WriteLine("");
+                }
+            }
         }
     }
 }
