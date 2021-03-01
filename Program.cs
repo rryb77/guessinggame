8fb7eba37a9aacfbc6424e5c2ad53487795e038e
Ryan Youngblood <rryb77@gmail.com>
2021-03-01 14:38:46 -0600

    Basic functionality added, phase 1 & 2

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace guessinggame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int secretNumber = 42;
+
+            Console.Write("Can you guess the secret number? ");
+            string input = Console.ReadLine();
+            int parsedInput = Int32.Parse(input);
+
+            if (parsedInput == secretNumber)
+            {
+                Console.WriteLine($"Correct! It was {secretNumber}!");
+            }
+            else
+            {
+                Console.WriteLine($"Wrong! It was {secretNumber}!");
+            }
+        }
+    }
+}
